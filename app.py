944413aa14e530bae1761717f01ffa9f9a44ad15
jmitteli <jmitteli24@students.oamk.fi>
2025-11-09 20:00:56 +0200

    Added ASCII art etc

--- app.py
+++ app.py
@@ -5,34 +5,56 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Yhdistetään MySQL/MariaDB:hen
+    # Connecting to MySQL/MariaDB server
     conn = mysql.connector.connect(
         host="localhost",
         user="exampleuser",
         password="change_this_strong_password",
         database="exampledb"
     )
-
     cursor = conn.cursor()
-
-    # Haetaan MySQL-palvelimen kellonaika
     cursor.execute("SELECT NOW()")
     db_time = cursor.fetchone()[0]
-
     cursor.close()
     conn.close()
 
-    # Personoitu etusivu
+    lemp_ascii = r"""
+██╗       ███████╗ ███╗   ███╗ ██████╗ 
+██║       ██╔════╝ ████╗ ████║ ██╔══██╗
+██║       █████╗   ██╔████╔██║ ██████╔╝
+██║       ██╔══╝   ██║╚██╔╝██║ ██╔═══╝ 
+███████╗  ███████╗ ██║ ╚═╝ ██║ ██║     
+╚══════╝  ╚══════╝ ╚═╝     ╚═╝ ╚═╝     
+"""
+
     return f"""
     <html>
       <head>
         <title>LEMP-demo</title>
+        <style>
+          body {{
+            font-family: Arial, sans-serif;
+            margin: 40px;
+            background-color: #f2f2f2; /* vaaleanharmaa tausta */
+          }}
+          .ascii {{
+            font-family: monospace;
+            font-size: 18px;
+            font-weight: 900; /* paksumpi */
+            line-height: 1.1;
+            white-space: pre;
+            margin-top: 24px;
+          }}
+        </style>
       </head>
-      <body style="font-family: Arial; margin: 40px;">
+      <body>
         <h1>Welcome to my LEMP-page!</h1>
+        <h1>Kotitehtävä 2</h1>
         <p>This page is running LEMP-stack on Nginx + MySQL + Flask!</p>
         <p><strong>MySQL-server time is:</strong> {db_time}</p>
-        <p><strong>MySQL server timezone is not set to Helsinki!!!!</p>
+        <p><strong>MySQL server timezone is not set to Helsinki!!!!</strong></p>
+
+        <div class="ascii">{lemp_ascii}</div>
       </body>
     </html>
     """
